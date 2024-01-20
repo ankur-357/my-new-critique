7cd48fe4f85dba2e0f690fa9ca95b22b2932ae34
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-01-20 15:18:29 +0530

    Update url.js

--- client/src/config/url.js
+++ client/src/config/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+//export const BASE_URL = "http://localhost:5000";
 
-//export const BASE_URL = "https://placetalks.cyclic.app";
+export const BASE_URL = "https://critiquecraze.vercel.app";
