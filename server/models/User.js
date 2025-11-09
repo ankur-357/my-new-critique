3eaddf5618e94530578433f23adf9b26bc90e6d7
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2025-11-10 05:24:34 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -6,12 +6,13 @@ const UserSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, "Username is required"],
-      unique: true,
+      trim: true,
     },
     email: {
       type: String,
       required: [true, "Email is required"],
       unique: true,
+      lowercase: true,
       validate: {
         validator: validator.isEmail,
         message: "Please enter a valid email address",
@@ -20,28 +21,37 @@ const UserSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: 5,
+      minlength: [5, "Password must be at least 5 characters long"],
     },
     profileImage: {
       type: String,
       default:
         "https://placetalks.s3.ap-south-1.amazonaws.com/istockphoto-1305665241-1024x1024.jpg",
     },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
     createdAt: {
       type: Date,
       default: Date.now,
     },
   },
   {
-    toJSON: {
-      virtuals: true,
-    },
-    toObject: {
-      virtuals: true,
-    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
 
+// Virtual field to populate places created by the user
 UserSchema.virtual("places", {
   ref: "Place",
   localField: "_id",
